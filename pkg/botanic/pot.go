74cc2a26c34ab1c5623ca10b4a1012cd890c7e28
Pablo Baños López <pbl1986@gmail.com>
2017-07-15 19:49:14 +0200

    Make pkg/botanic.Pot.Grow return an additional error value

--- pkg/botanic/pot.go
+++ pkg/botanic/pot.go
@@ -1,6 +1,6 @@
 package botanic
 
-import "sync"
+import "fmt"
 
 /*
 Pot represents the context in which a tree is grown.
@@ -8,7 +8,7 @@ Pot represents the context in which a tree is grown.
 Its Grow method takes a Set and returns a tree that predicts the set
 */
 type Pot interface {
-	Grow(Set) *Tree
+	Grow(Set) (*Tree, error)
 }
 
 type pot struct {
@@ -16,61 +16,77 @@ type pot struct {
 	classFeature   Feature
 	minimumEntropy float64
 	pruner         Pruner
+	maxConcurrency int
 }
 
 /*
 New takes a slice of features, a feature class and a pruner and returns a Pot that
 uses those to grow a tree
 */
-func New(features []Feature, fc Feature, p Pruner) Pot {
-	return &pot{features, fc, 0.0, p}
+func New(features []Feature, fc Feature, p Pruner, maxConcurrency int) Pot {
+	return &pot{features, fc, 0.0, p, maxConcurrency}
 }
 
-func (p *pot) Grow(s Set) *Tree {
+func (p *pot) Grow(s Set) (*Tree, error) {
 	t := &Tree{set: s}
-	p.develop(t, p.features)
-	return t
+	q := newQueue(p.maxConcurrency)
+	q.add(p, t, p.features)
+	err := q.waitForAll()
+	return t, err
 }
 
-func (p *pot) develop(t *Tree, features []Feature) {
-	t.prediction, _ = newPredictionFromSet(t.set, p.classFeature)
-	if len(features) == 0 || t.set.Entropy(p.classFeature) <= p.minimumEntropy {
-		return
+func (p *pot) develop(t *Tree, features []Feature, q *queue) error {
+	prediction, err := newPredictionFromSet(t.set, p.classFeature)
+	if err != nil {
+		if err != ErrCannotPredictFromEmptySet {
+			return err
+		}
+	}
+	t.prediction = prediction
+	sEntropy, err := t.set.Entropy(p.classFeature)
+	if err != nil {
+		return err
+	}
+	if len(features) == 0 || sEntropy <= p.minimumEntropy {
+		return nil
 	}
 	var partition *Partition
 	var featureIndex int
 	for i, f := range features {
-		p := p.partition(t.set, f, p.classFeature)
+		p, err := p.partition(t.set, f, p.classFeature)
+		if err != nil {
+			return err
+		}
 		if partition == nil || (p != nil && p.informationGain > partition.informationGain) {
 			partition = p
 			featureIndex = i
 		}
 	}
 	if partition == nil {
-		return
+		return nil
 	}
 	t.subtreeFeature = partition.feature
 	t.informationGain = partition.informationGain
 	t.subtrees = partition.subtrees
+	subtreeFeatures := make([]Feature, 0, len(features)-1)
+	for fi, sf := range features {
+		if fi != featureIndex {
+			subtreeFeatures = append(subtreeFeatures, sf)
+		}
+	}
+	for _, subtree := range t.subtrees {
+		q.add(p, subtree, subtreeFeatures)
+	}
 	t.undefinedSubtree = NewTreeForUndefinedFeatureCriterion(partition.feature, t.set)
 	t.set = nil
-	features[featureIndex], features[0] = features[0], features[featureIndex]
-	var wg sync.WaitGroup
-	wg.Add(len(t.subtrees) + 1)
-	for _, subtree := range append(t.subtrees, t.undefinedSubtree) {
-		go func(subtree *Tree) {
-			p.develop(subtree, append([]Feature{}, features[1:]...))
-			wg.Done()
-		}(subtree)
-	}
-	wg.Wait()
-	features[featureIndex], features[0] = features[0], features[featureIndex]
+	q.add(p, t.undefinedSubtree, subtreeFeatures)
+	return nil
 }
 
-func (p *pot) partition(s Set, f Feature, cf Feature) *Partition {
+func (p *pot) partition(s Set, f Feature, cf Feature) (*Partition, error) {
 	switch f := f.(type) {
 	default:
-		return nil
+		return nil, fmt.Errorf("unknown feature type %T", f)
 	case *DiscreteFeature:
 		return NewDiscretePartition(s, f, cf, p.pruner)
 	case *ContinuousFeature:
